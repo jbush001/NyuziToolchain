7b3a8de4779cbd12edfb14e194b2b9dafc110687
Jeff Bush <jeffbush001@gmail.com>
2013-06-30 12:11:19 -0700

    Fix bug where getting lane of floating point vector didn't work (didn't match any forms).

--- test/CodeGen/VectorProc/extractelement.ll
+++ test/CodeGen/VectorProc/extractelement.ll
@@ -2,9 +2,18 @@
 
 target triple = "vectorproc"
 
-define i32 @test(<16 x i32> %a, i32 %b) #0 {
+define i32 @testi(<16 x i32> %a, i32 %b) {	; CHECK: testi:
 	%elem = extractelement <16 x i32> %a, i32 %b
+	
+	; CHECK: s{{[0-9]+}} = getfield(v{{[0-9]+}}, s{{[0-9]+}})
+
 	ret i32 %elem
 }
 
-; CHECK: s{{[0-9]+}} = getfield(v{{[0-9]+}}, s{{[0-9]+}})
+define float @testf(<16 x float> %a, i32 %b) {	; CHECK: testf:
+	%elem = extractelement <16 x float> %a, i32 %b
+	
+	; CHECK: f{{[0-9]+}} = getfield(vf{{[0-9]+}}, s{{[0-9]+}})
+
+	ret float %elem
+}
--- tools/clang/test/CodeGen/vectorproc-vops.c
+++ tools/clang/test/CodeGen/vectorproc-vops.c
@@ -1,15 +1,21 @@
 // RUN: %clang %s -O3 -target vectorproc -S -o - | FileCheck %s
 
 typedef int veci16 __attribute__((__vector_size__(16 * sizeof(int))));
+typedef float vecf16 __attribute__((__vector_size__(16 * sizeof(float))));
 
-
-veci16 test_vadd(veci16 a, veci16 b)	// CHECK: test_vadd:
+veci16 test_vaddi(veci16 a, veci16 b)	// CHECK: test_vaddi:
 {
 	return a + b;
 	// CHECK: v{{[0-9]+}} = v0 + v1
 }
 
-int sum_lanes(veci16 a)	// CHECK: sum_lanes:
+vecf16 test_vaddf(vecf16 a, vecf16 b)	// CHECK: test_vaddf:
+{
+	return a + b;
+	// CHECK: vf{{[0-9]+}} = vf0 + vf1
+}
+
+int sum_lanesi(veci16 a)	// CHECK: sum_lanesi:
 {
 	int sum = 0;
 	
@@ -22,12 +28,33 @@ int sum_lanes(veci16 a)	// CHECK: sum_lanes:
 	return sum;
 }
 
-veci16 vector_scalar(veci16 a, int b)	// CHECK: vector_scalar:
+float sum_lanesf(vecf16 a)	// CHECK: sum_lanesf:
+{
+	float sum = 0;
+	
+	for (int index = 0; index < 16; index++)
+	{
+		sum += a[index];
+		// CHECK: f{{[0-9]+}} = getfield(vf0, s{{[0-9]+}})
+	}
+
+	return sum;
+}
+
+veci16 vector_scalari(veci16 a, int b)	// CHECK: vector_scalari:
 {
 	return a + __builtin_vp_makevectori(b);
 	
 	// CHECK: v0 = v0 + s0
 }
+
+vecf16 vector_scalarf(vecf16 a, float b)	// CHECK: vector_scalarf:
+{
+	return a + __builtin_vp_makevectorf(b);
+	
+	// CHECK: vf0 = vf0 + f0
+}
+
 
 veci16 vector_scalar_const(veci16 a)	// CHECK: vector_scalar_const
 {
