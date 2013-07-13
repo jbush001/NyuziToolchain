b7ff74b99b1656ab4aa4e77ce8d66063f7e07350
Jeff Bush <jeffbush001@gmail.com>
2013-07-13 07:13:04 -0700

    Properly fixup call instructions

--- test/MC/VectorProc/branch.s
+++ test/MC/VectorProc/branch.s
@@ -5,4 +5,10 @@ foo:	goto foo		; CHECK: encoding: [0bAAA00000,A,A,0b1111011A]
 		
 		btrue s1, foo	; CHECK: encoding: [0bAAA00001,A,A,0b1111010A]
 		; CHECK: ;   fixup A - offset: 0, value: foo, kind: fixup_VectorProc_PCRel_Branch		
+
+		call bar	; CHECK: encoding: [0bAAA00000,A,A,0b1111100A]
+		; CHECK: ;   fixup A - offset: 0, value: bar, kind: fixup_VectorProc_PCRel_Branch		
+
+	
+bar:
 		
