336c55eb32b92d5d36331ca75000b9476a8cc3fd
Jeff Bush <jeffbush001@gmail.com>
2013-12-16 22:20:16 -0800

    Check lea fixup kind

--- test/MC/VectorProc/misc.s
+++ test/MC/VectorProc/misc.s
@@ -4,7 +4,11 @@ foo: .long bar
 	load_32 s4, foo		# CHECK: encoding: [0x9f,0bAAAAAA00,A,0b1010100A]
 		# CHECK: fixup A - offset: 0, value: foo, kind: fixup_VectorProc_PCRel_MemAccExt
 
+baz: .long 12
   # Load effective address of label (this converts to add_i s5, pc, <offset to label>)	
-  lea s5, foo  # CHECK: encoding: [0xbf,0bAAAAAA00,0b1AAAAAAA,0x02]
+  lea s5, baz  # CHECK: encoding: [0xbf,0bAAAAAA00,0b1AAAAAAA,0x02]
+		# CHECK: fixup A - offset: 0, value: baz, kind: fixup_VectorProc_PCRel_ComputeLabelAddress
 
+bar:
   nop # CHECK: encoding: [0x00,0x00,0x00,0x00]
+  
