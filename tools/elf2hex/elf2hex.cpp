ef12d8957916f6c8d85ca22c21a84e9317ef423f
Jeff Bush <jeffbush001@gmail.com>
2013-12-19 08:18:22 -0800

    Update elf2hex to use more LLVM library functions.

--- tools/elf2hex/elf2hex.cpp
+++ tools/elf2hex/elf2hex.cpp
@@ -1,10 +1,14 @@
+
 //
 // This tool is specific to the GPGPU target.
 // Convert a statically linked ELF executable into its in-memory representation
 // and write it out in hex format compatible with a Verilog simulator's $readmemh
 // function.
+// Jeff Bush 2013
 //
 
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/Debug.h"
 #include <llvm/Support/Compiler.h>
 #include <llvm/Support/ELF.h>
 #include <stdio.h>
@@ -12,40 +16,35 @@
 #include <unistd.h>
 
 using namespace llvm::ELF;
+using namespace llvm;
+
+static cl::opt<std::string>
+InputFilename(cl::Positional, cl::desc("<input ELF file>"), cl::init("a.out"));
+
+static cl::opt<std::string>
+OutputFilename("o", cl::desc("Output hex file"), cl::value_desc("a.hex"));
 
 int main(int argc, const char *argv[])
 {
-	FILE *inputFile;
-	FILE *outputFile;
-	Elf32_Ehdr eheader;
-	Elf32_Phdr *pheader;
-	int totalSize;
-	int segment;
-	unsigned char *result;
-	int i;
-	
-	if (argc != 3)
-	{
-		fprintf(stderr, "USAGE: <dest> <src>\n");
-		return 1;
-	}
+  cl::ParseCommandLineOptions(argc, argv, "elf2hex converter\n");
 
-	inputFile = fopen(argv[2], "rb");
+	FILE *inputFile = fopen(InputFilename.c_str(), "rb");
 	if (!inputFile)
 	{
-		perror("error opening input file");
+		errs() << "error opening input file";
 		return 1;
 	}
 
+	ELF::Elf32_Ehdr eheader;
 	if (fread(&eheader, sizeof(eheader), 1, inputFile) != 1)
 	{
-		perror("error reading header");
+		errs() << "error reading header";
 		return 1;
 	}
 	
 	if (memcmp(eheader.e_ident, ElfMagic, 4) != 0) 
 	{
-		fprintf(stderr, "not an elf file\n");
+    errs() << "not an elf file\n";
 		return 1;
 	}
 	
@@ -55,52 +54,52 @@ int main(int argc, const char *argv[])
 		return 1;
 	}
 
-	pheader = (Elf32_Phdr*) calloc(sizeof(Elf32_Phdr), eheader.e_phnum);
+	Elf32_Phdr *pheader = (Elf32_Phdr*) calloc(sizeof(Elf32_Phdr), eheader.e_phnum);
 	fseek(inputFile, eheader.e_phoff, SEEK_SET);
 	if (fread(pheader, sizeof(Elf32_Phdr), eheader.e_phnum, inputFile) != eheader.e_phnum)
 	{
-		perror("reading program header");
+    errs() << "error reading program header\n";
 		return 1;
 	}
 
-	totalSize = pheader[eheader.e_phnum - 1].p_vaddr 
+	int totalSize = pheader[eheader.e_phnum - 1].p_vaddr 
 		+ pheader[eheader.e_phnum - 1].p_memsz; 
-	result = (unsigned char*) calloc(totalSize, 1);
-	if (!result)
+	unsigned char *memoryImage = (unsigned char*) calloc(totalSize, 1);
+	if (!memoryImage)
 	{
-		fprintf(stderr, "not enough memory\n");
+		errs() << "not enough memory for program image\n";
 		return 1;
 	}
 
-	for (segment = 0; segment < eheader.e_phnum; segment++)
+	for (int segment = 0; segment < eheader.e_phnum; segment++)
 	{
 		if (pheader[segment].p_type == PT_LOAD)
 		{
 			fseek(inputFile, pheader[segment].p_offset, SEEK_SET);
-			if (fread(result + pheader[segment].p_vaddr, 1, pheader[segment].p_filesz,
+			if (fread(memoryImage + pheader[segment].p_vaddr, 1, pheader[segment].p_filesz,
 				inputFile) != pheader[segment].p_filesz)
 			{
-				printf("Error reading segment\n");
+				errs() << "Error reading segment " << segment << "\n";
 				return 1;
 			}
 		}
 	}
 
 	// Convert the first word into a jump instruction to the appropriate location
-	*((unsigned int*) result) = 0xf6000000 | ((eheader.e_entry - 4) << 5);
+	*((unsigned int*) memoryImage) = 0xf6000000 | ((eheader.e_entry - 4) << 5);
 		
 	fclose(inputFile);
 	
-	outputFile = fopen(argv[1], "wb");
+	FILE *outputFile = fopen(OutputFilename.c_str(), "wb");
 	if (!outputFile)
 	{
-		perror("error opening output file");
+		errs() << "error opening output file";
 		return 1;
 	}
 	
-	for (i = 0; i < totalSize; i++)
+	for (int i = 0; i < totalSize; i++)
 	{
-		fprintf(outputFile, "%02x", result[i]);
+		fprintf(outputFile, "%02x", memoryImage[i]);
 		if ((i & 3) == 3)
 			fprintf(outputFile, "\n");	
 	}
