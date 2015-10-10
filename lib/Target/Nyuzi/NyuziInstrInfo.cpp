jbush001/NyuziToolchain

commit bee0b24b7bef8de7b3d372f2e249bbe53dd35e9a

work in progress adding instruction scheduling itinerary
- CPU name was not being passed to backend properly. Updated clang to
do this, and assert in backend if it is not set.
- Switch from legacy selectiondag scheduler to MachineScheduler by making
NyuziSubtarget::enableMachineScheduler return true.
- Add scheduler tablegen file and specify instruction itineraries in
instruction info.

This is not yet working. The instruction latency data appears to be
passed to ScoreboardHazardRecognizer, but it doesn't seem to change
the generated code.

lib/Target/Nyuzi/Nyuzi.td
@@ -19,6 +19,7 @@ include "llvm/Target/Target.td"
 
 include "NyuziRegisterInfo.td"
 include "NyuziCallingConv.td"
+include "NyuziSchedule.td"
 include "NyuziInstrFormats.td"
 include "NyuziInstrInfo.td"
 
@@ -33,10 +34,7 @@ def NyuziAsmWriter : AsmWriter {
   bit isMCAsmWriter = 1;
 }
 
-class Proc<string Name, list<SubtargetFeature> Features>
- : Processor<Name, NoItineraries, Features>;
-
-def : Proc<"generic", []>;
+def : Processor<"nyuzi", NyuziItineraries, []>;
 
 def Nyuzi : Target {
   // Pull in Instruction Info:

lib/Target/Nyuzi/NyuziSubtarget.cpp
@@ -33,12 +33,13 @@ NyuziSubtarget::NyuziSubtarget(const Triple &TT, const std::string &CPU,
       TLInfo(NyuziTargetLowering::create(TM, *this)), TSInfo(),
       FrameLowering(NyuziFrameLowering::create(*this)) {
 
-  // Determine default and user specified characteristics
-  std::string CPUName = CPU;
-  if (CPUName.empty()) {
-    CPUName = "nyuzi";
-  }
+  // If the CPU name is empty, a number of other things will not work correctly
+  // in subtle ways.
+  assert(!CPU.empty());
+
+  InstrItins = getInstrItineraryForCPU(CPU);
+  assert(!InstrItins.isEmpty());
 
   // Parse features string.
-  ParseSubtargetFeatures(CPUName, FS);
+  ParseSubtargetFeatures(CPU, FS);
 }

lib/Target/Nyuzi/NyuziSubtarget.h
@@ -34,6 +34,7 @@ class NyuziSubtarget : public NyuziGenSubtargetInfo {
   std::unique_ptr<const NyuziTargetLowering> TLInfo;
   NyuziSelectionDAGInfo TSInfo;
   std::unique_ptr<const NyuziFrameLowering> FrameLowering;
+  InstrItineraryData InstrItins;
 
 public:
   NyuziSubtarget(const Triple &TT, const std::string &CPU,
@@ -56,6 +57,15 @@ public:
   }
   virtual const NyuziSelectionDAGInfo *getSelectionDAGInfo() const override {
     return &TSInfo;
+  }
+  bool enableMachineScheduler() const override {
+    return true;
+  }
+  bool enablePostRAScheduler() const override {
+    return true;
+  }
+  const InstrItineraryData *getInstrItineraryData() const override {
+    return &InstrItins;
   }
 };
 

lib/Target/Nyuzi/NyuziInstrFormats.td
@@ -114,7 +114,7 @@ def call       : SDNode<"NyuziISD::CALL", SDT_SPCall,
 // Instruction Classes
 //////////////////////////////////////////////////////////////////
 
-class NyuziInstruction<dag outputs, dag inputs, string asmString, list<dag> pattern>
+class NyuziInstruction<dag outputs, dag inputs, string asmString, list<dag> pattern, InstrItinClass itin>
 	: Instruction 
 {
 	let Namespace = "Nyuzi";
@@ -123,13 +123,14 @@ class NyuziInstruction<dag outputs, dag inputs, string asmString, list<dag> patt
 	let AsmString = asmString;
 	let Pattern = pattern;
 	let Size = 4;
+	let Itinerary = itin;
 
 	field bits<32> Inst;
 	field bits<32> SoftFail = 0;	// needed for disassembler
 }
 
 class Pseudo<dag outputs, dag inputs, list<dag> pattern>
-	: NyuziInstruction<outputs, inputs, "Pseudo", pattern>
+	: NyuziInstruction<outputs, inputs, "Pseudo", pattern, II_PSEUDO>
 {
 	let isCodeGenOnly = 1;
 	let isPseudo = 1;
@@ -164,8 +165,8 @@ def FmtR_VVV : FmtR<4>;
 def FmtR_VVVM : FmtR<5>;
 
 class FormatRInst<dag outputs, dag inputs, string asmString, list<dag> pattern,
-	bits<6> opcode, FmtR fmt> 
-	: NyuziInstruction<outputs, inputs, asmString, pattern>
+	bits<6> opcode, FmtR fmt, InstrItinClass itin> 
+	: NyuziInstruction<outputs, inputs, asmString, pattern, itin>
 {
 	bits <5> dest;
 	bits <5> src2;
@@ -178,15 +179,15 @@ class FormatRInst<dag outputs, dag inputs, string asmString, list<dag> pattern,
 }
 
 class FormatRUnmaskedOneOpInst<dag outputs, dag inputs, string asmString, list<dag> pattern,
-	bits<6> opcode, FmtR fmt> 
-	: FormatRInst<outputs, inputs, asmString, pattern, opcode, fmt>
+	bits<6> opcode, FmtR fmt, InstrItinClass itin> 
+	: FormatRInst<outputs, inputs, asmString, pattern, opcode, fmt, itin>
 {
 	let Inst{4-0} = 0;
 }
 
 class FormatRMaskedOneOpInst<dag outputs, dag inputs, string asmString, list<dag> pattern,
-	bits<6> opcode, FmtR fmt> 
-	: FormatRInst<outputs, inputs, asmString, pattern, opcode, fmt>
+	bits<6> opcode, FmtR fmt, InstrItinClass itin> 
+	: FormatRInst<outputs, inputs, asmString, pattern, opcode, fmt, itin>
 {
 	bits <5> mask;
 	let Inst{14-10} = mask;
@@ -194,16 +195,16 @@ class FormatRMaskedOneOpInst<dag outputs, dag inputs, string asmString, list<dag
 }
 
 class FormatRUnmaskedTwoOpInst<dag outputs, dag inputs, string asmString, list<dag> pattern,
-	bits<6> opcode, FmtR fmt> 
-	: FormatRInst<outputs, inputs, asmString, pattern, opcode, fmt>
+	bits<6> opcode, FmtR fmt, InstrItinClass itin> 
+	: FormatRInst<outputs, inputs, asmString, pattern, opcode, fmt, itin>
 {
 	bits <5> src1;
 	let Inst{4-0} = src1;
 }
 
 class FormatRMaskedTwoOpInst<dag outputs, dag inputs, string asmString, list<dag> pattern,
-	bits<6> opcode, FmtR fmt> 
-	: FormatRInst<outputs, inputs, asmString, pattern, opcode, fmt>
+	bits<6> opcode, FmtR fmt, InstrItinClass itin> 
+	: FormatRInst<outputs, inputs, asmString, pattern, opcode, fmt, itin>
 {
 	bits <5> src1;
 	bits <5> mask;
@@ -226,8 +227,8 @@ def FmtI_VS : FmtI<4>;
 def FmtI_VSM : FmtI<5>;
 
 class FormatIInst<dag outputs, dag inputs, string asmString, list<dag> pattern,
-	bits<5> opcode, FmtI fmt>  
-	: NyuziInstruction<outputs, inputs, asmString, pattern> 
+	bits<5> opcode, FmtI fmt, InstrItinClass itin>  
+	: NyuziInstruction<outputs, inputs, asmString, pattern, itin> 
 {
 	bits <5> dest;
 	bits <5> src1;
@@ -240,8 +241,8 @@ class FormatIInst<dag outputs, dag inputs, string asmString, list<dag> pattern,
 }
 
 class FormatIMaskedInst<dag outputs, dag inputs, string asmString, list<dag> pattern,
-	bits<5> opcode, FmtI fmt> 
-	: FormatIInst<outputs, inputs, asmString, pattern, opcode, fmt> 
+	bits<5> opcode, FmtI fmt, InstrItinClass itin> 
+	: FormatIInst<outputs, inputs, asmString, pattern, opcode, fmt, itin> 
 {
 	bits <5> mask;
 	bits <8> imm;
@@ -250,8 +251,8 @@ class FormatIMaskedInst<dag outputs, dag inputs, string asmString, list<dag> pat
 }
 
 class FormatIUnmaskedInst<dag outputs, dag inputs, string asmString, list<dag> pattern,
-	bits<5> opcode, FmtI fmt> 
-	: FormatIInst<outputs, inputs, asmString, pattern, opcode, fmt> 
+	bits<5> opcode, FmtI fmt, InstrItinClass itin> 
+	: FormatIInst<outputs, inputs, asmString, pattern, opcode, fmt, itin> 
 {
 	bits <13> imm;
 	let Inst{22-10} = imm;
@@ -266,7 +267,8 @@ multiclass TwoOpIntArith<string operator, SDNode OpNode, bits<6> opcode> {
 		operator # " $dest, $src1, $src2",
 		[(set i32:$dest, (OpNode i32:$src1, i32:$src2))],
 		opcode,
-		FmtR_SSS>;
+		FmtR_SSS, 
+		II_INT>;
 		
 	// Vector = Vector Op Vector
 	def VVV : FormatRUnmaskedTwoOpInst<
@@ -275,7 +277,8 @@ multiclass TwoOpIntArith<string operator, SDNode OpNode, bits<6> opcode> {
 		operator # " $dest, $src1, $src2",
 		[(set v16i32:$dest, (OpNode v16i32:$src1, v16i32:$src2))],
 		opcode,
-		FmtR_VVV>;
+		FmtR_VVV, 
+		II_INT>;
 
 	// Vector = Vector Op Scalar
 	def VVS : FormatRUnmaskedTwoOpInst<
@@ -284,7 +287,8 @@ multiclass TwoOpIntArith<string operator, SDNode OpNode, bits<6> opcode> {
 		operator # " $dest, $src1, $src2",
 		[(set v16i32:$dest, (OpNode v16i32:$src1, (splat i32:$src2)))],
 		opcode,
-		FmtR_VVS>;
+		FmtR_VVS,
+		II_INT>;
 
 	let Constraints = "$dest = $oldvalue" in {
 		// Vector = Vector op Vector, masked
@@ -294,7 +298,8 @@ multiclass TwoOpIntArith<string operator, SDNode OpNode, bits<6> opcode> {
 			operator # "_mask $dest, $mask, $src1, $src2",
 			[(set v16i32:$dest, (int_nyuzi_vector_mixi i32:$mask, (OpNode v16i32:$src1, v16i32:$src2), v16i32:$oldvalue))],
 			opcode,
-			FmtR_VVVM>;
+			FmtR_VVVM,
+			II_INT>;
 
 		// Vector = Vector Op Scalar, masked
 		def VVSM : FormatRMaskedTwoOpInst<
@@ -303,7 +308,8 @@ multiclass TwoOpIntArith<string operator, SDNode OpNode, bits<6> opcode> {
 			operator # "_mask $dest, $mask, $src1, $src2",
 			[(set v16i32:$dest, (int_nyuzi_vector_mixi i32:$mask, (OpNode v16i32:$src1, (splat i32:$src2)), v16i32:$oldvalue))],
 			opcode,
-			FmtR_VVSM>;
+			FmtR_VVSM,
+			II_INT>;
 	}
 
 	// Format I
@@ -314,7 +320,8 @@ multiclass TwoOpIntArith<string operator, SDNode OpNode, bits<6> opcode> {
 		operator # " $dest, $src1, $imm",
 		[(set i32:$dest, (OpNode i32:$src1, (i32 simm13:$imm)))],
 		opcode{4-0},
-		FmtI_SS>;
+		FmtI_SS,
+		II_INT>;
 	
 	// Vector = Vector Op Immediate
 	def VVI : FormatIUnmaskedInst<
@@ -323,7 +330,8 @@ multiclass TwoOpIntArith<string operator, SDNode OpNode, bits<6> opcode> {
 		operator # " $dest, $src1, $imm",
 		[(set v16i32:$dest, (OpNode v16i32:$src1, (splat simm13:$imm)))],
 		opcode{4-0},
-		FmtI_VV>;
+		FmtI_VV,
+		II_INT>;
 
 	// Vector = Scalar Op Immediate
 	def VSI : FormatIUnmaskedInst<
@@ -332,7 +340,8 @@ multiclass TwoOpIntArith<string operator, SDNode OpNode, bits<6> opcode> {
 		operator # " $dest, $src1, $imm",
 		[(set v16i32:$dest, (OpNode (splat i32:$src1), (splat simm13:$imm)))],
 		opcode{4-0},
-		FmtI_VS>;
+		FmtI_VS,
+		II_INT>;
 
 	let Constraints = "$dest = $oldvalue" in {
 		// Vector = Vector Op Immediate, masked
@@ -342,7 +351,8 @@ multiclass TwoOpIntArith<string operator, SDNode OpNode, bits<6> opcode> {
 			operator # "_mask $dest, $mask, $src1, $imm",
 			[(set v16i32:$dest, (int_nyuzi_vector_mixi i32:$mask, (OpNode v16i32:$src1, (splat simm8:$imm)), v16i32:$oldvalue))],
 			opcode{4-0},
-			FmtI_VVM>;
+			FmtI_VVM,
+			II_INT>;
 
 		// Vector = Scalar Op Immediate, masked
 		def VSIM : FormatIMaskedInst<
@@ -351,7 +361,8 @@ multiclass TwoOpIntArith<string operator, SDNode OpNode, bits<6> opcode> {
 			operator # "_mask $dest, $mask, $src1, $imm",
 			[(set v16i32:$dest, (int_nyuzi_vector_mixi i32:$mask, (OpNode (splat i32:$src1), (splat simm8:$imm)), v16i32:$oldvalue))],
 			opcode{4-0},
-			FmtI_VSM>;
+			FmtI_VSM,
+			II_INT>;
 	}
 }
 
@@ -364,7 +375,8 @@ multiclass TwoOpFloatArith<string operator, SDNode OpNode, bits<6> opcode>
 		operator # " $dest, $src1, $src2",
 		[(set GPR32:$dest, (OpNode f32:$src1, f32:$src2))],
 		opcode,
-		FmtR_SSS>;
+		FmtR_SSS,
+		II_FLOAT>;
 
 	// Vector = Vector Op Vector
 	def VVV : FormatRUnmaskedTwoOpInst<
@@ -373,7 +385,8 @@ multiclass TwoOpFloatArith<string operator, SDNode OpNode, bits<6> opcode>
 		operator # " $dest, $src1, $src2",
 		[(set VR512:$dest, (OpNode v16f32:$src1, v16f32:$src2))],
 		opcode,
-		FmtR_VVV>;
+		FmtR_VVV,
+		II_FLOAT>;
 
 	// Vector = Vector Op Scalar
 	def VVS : FormatRUnmaskedTwoOpInst<
@@ -382,7 +395,8 @@ multiclass TwoOpFloatArith<string operator, SDNode OpNode, bits<6> opcode>
 		operator # " $dest, $src1, $src2",
 		[(set VR512:$dest, (OpNode v16f32:$src1, (splat f32:$src2)))],
 		opcode,
-		FmtR_VVS>;
+		FmtR_VVS,
+		II_FLOAT>;
 
 	// Predicated
 	let Constraints = "$dest = $oldvalue" in {
@@ -394,7 +408,8 @@ multiclass TwoOpFloatArith<string operator, SDNode OpNode, bits<6> opcode>
 			[(set VR512:$dest, (int_nyuzi_vector_mixf i32:$mask, (OpNode v16f32:$src1, 
 				v16f32:$src2), v16f32:$oldvalue))],
 			opcode,
-			FmtR_VVVM>;
+			FmtR_VVVM,
+			II_FLOAT>;
 		
 		// Vector = Vector Op Scalar, masked
 		def VVSM : FormatRMaskedTwoOpInst<
@@ -404,7 +419,8 @@ multiclass TwoOpFloatArith<string operator, SDNode OpNode, bits<6> opcode>
 			[(set v16f32:$dest, (int_nyuzi_vector_mixf i32:$mask, (OpNode v16f32:$src1, 
 				(splat f32:$src2)), v16f32:$oldvalue))],
 			opcode,
-			FmtR_VVSM>;
+			FmtR_VVSM,
+			II_FLOAT>;
 	}
 }
 
@@ -415,7 +431,8 @@ multiclass OneOpIntArith<string operator, SDNode OpNode, bits<6> opcode> {
 		operator # " $dest, $src2",
 		[(set i32:$dest, (OpNode i32:$src2))],
 		opcode,
-		FmtR_SSS>;
+		FmtR_SSS,
+		II_INT>;
 
 	def VS : FormatRUnmaskedOneOpInst<
 		(outs VR512:$dest), 
@@ -423,7 +440,8 @@ multiclass OneOpIntArith<string operator, SDNode OpNode, bits<6> opcode> {
 		operator # " $dest, $src2",
 		[(set v16i32:$dest, (OpNode (splat i32:$src2)))],
 		opcode,
-		FmtR_VVS>;
+		FmtR_VVS,
+		II_INT>;
 
 	def VV : FormatRUnmaskedOneOpInst<
 		(outs VR512:$dest), 
@@ -431,7 +449,8 @@ multiclass OneOpIntArith<string operator, SDNode OpNode, bits<6> opcode> {
 		operator # " $dest, $src2",
 		[(set v16i32:$dest, (OpNode v16i32:$src2))],
 		opcode,
-		FmtR_VVV>;
+		FmtR_VVV,
+		II_INT>;
 
 	// Predicated
 	let Constraints = "$dest = $oldvalue" in {
@@ -442,7 +461,8 @@ multiclass OneOpIntArith<string operator, SDNode OpNode, bits<6> opcode> {
 			[(set v16i32:$dest, (int_nyuzi_vector_mixi i32:$mask, (OpNode v16i32:$src2), 
 				v16i32:$oldvalue))],
 			opcode,
-			FmtR_VVVM>;
+			FmtR_VVVM,
+			II_INT>;
 
 		def VSM : FormatRMaskedOneOpInst<
 			(outs VR512:$dest),
@@ -451,7 +471,8 @@ multiclass OneOpIntArith<string operator, SDNode OpNode, bits<6> opcode> {
 			[(set v16i32:$dest, (int_nyuzi_vector_mixi i32:$mask, (OpNode (splat i32:$src2)), 
 				v16i32:$oldvalue))],
 			opcode,
-			FmtR_VVSM>;
+			FmtR_VVSM,
+			II_INT>;
 	}
 }
 
@@ -462,7 +483,8 @@ multiclass OneOpFloatArith<string operator, SDNode OpNode, bits<6> opcode> {
 		operator # " $dest, $src2",
 		[(set f32:$dest, (OpNode f32:$src2))],
 		opcode,
-		FmtR_SSS>;
+		FmtR_SSS,
+		II_FLOAT>;
 
 	def VS : FormatRUnmaskedOneOpInst<
 		(outs VR512:$dest), 
@@ -470,7 +492,8 @@ multiclass OneOpFloatArith<string operator, SDNode OpNode, bits<6> opcode> {
 		operator # " $dest, $src2",
 		[(set v16f32:$dest, (OpNode (splat f32:$src2)))],
 		opcode,
-		FmtR_VVS>;
+		FmtR_VVS,
+		II_FLOAT>;
 
 	def VV : FormatRUnmaskedOneOpInst<
 		(outs VR512:$dest), 
@@ -478,7 +501,8 @@ multiclass OneOpFloatArith<string operator, SDNode OpNode, bits<6> opcode> {
 		operator # " $dest, $src2",
 		[(set v16f32:$dest, (OpNode v16f32:$src2))],
 		opcode,
-		FmtR_VVV>;
+		FmtR_VVV,
+		II_FLOAT>;
 
 	// Predicated
 	let Constraints = "$dest = $oldvalue" in {
@@ -489,7 +513,8 @@ multiclass OneOpFloatArith<string operator, SDNode OpNode, bits<6> opcode> {
 			[(set v16f32:$dest, (int_nyuzi_vector_mixf i32:$mask, (OpNode v16f32:$src2), 
 				v16f32:$oldvalue))],
 			opcode,
-			FmtR_VVVM>;
+			FmtR_VVVM,
+			II_FLOAT>;
 
 		def VSM : FormatRMaskedOneOpInst<
 			(outs VR512:$dest),
@@ -498,7 +523,8 @@ multiclass OneOpFloatArith<string operator, SDNode OpNode, bits<6> opcode> {
 			[(set v16f32:$dest, (int_nyuzi_vector_mixf i32:$mask, (OpNode (splat f32:$src2)), 
 				v16f32:$oldvalue))],
 			opcode,
-			FmtR_VVSM>;
+			FmtR_VVSM,
+			II_FLOAT>;
 	}
 }
 
@@ -511,7 +537,8 @@ multiclass IntCompareInst<string operator, CondCode condition,
 		operator # " $dest, $src1, $src2",
 		[(set i32:$dest, (setcc i32:$src1, i32:$src2, condition))],
 		opcode,
-		FmtR_SSS>;
+		FmtR_SSS,
+		II_INT>;
 
 	def VV : FormatRUnmaskedTwoOpInst<
 		(outs GPR32:$dest), 
@@ -519,7 +546,8 @@ multiclass IntCompareInst<string operator, CondCode condition,
 		operator # " $dest, $src1, $src2",
 		[(set i32:$dest, (vectorIntr v16i32:$src1, v16i32:$src2))],
 		opcode,
-		FmtR_VVV>;
+		FmtR_VVV,
+		II_INT>;
 
 	def VS : FormatRUnmaskedTwoOpInst<
 		(outs GPR32:$dest), 
@@ -527,7 +555,8 @@ multiclass IntCompareInst<string operator, CondCode condition,
 		operator # " $dest, $src1, $src2",
 		[(set i32:$dest, (vectorIntr v16i32:$src1, (splat i32:$src2)))],
 		opcode,
-		FmtR_VVS>;
+		FmtR_VVS,
+		II_INT>;
 
 	// Instruction format B
 	def SI : FormatIUnmaskedInst<
@@ -536,7 +565,8 @@ multiclass IntCompareInst<string operator, CondCode condition,
 		operator # " $dest, $src1, $imm",
 		[(set i32:$dest, (setcc i32:$src1, simm13:$imm, condition))],
 		opcode{4-0},
-		FmtI_SS>;
+		FmtI_SS,
+		II_INT>;
 
 	def VI : FormatIUnmaskedInst<
 		(outs GPR32:$dest), 
@@ -544,7 +574,8 @@ multiclass IntCompareInst<string operator, CondCode condition,
 		operator # " $dest, $src1, $imm",
 		[(set i32:$dest, (vectorIntr v16i32:$src1, (splat simm13:$imm)))],
 		opcode{4-0},
-		FmtI_VV>;
+		FmtI_VV,
+		II_INT>;
 }
 
 multiclass FloatCompareInst<string operator, CondCode condition, bits<6> opcode,
@@ -555,7 +586,8 @@ multiclass FloatCompareInst<string operator, CondCode condition, bits<6> opcode,
 		operator # "_f $dest, $src1, $src2",
 		[(set i32:$dest, (setcc f32:$src1, f32:$src2, condition))],
 		opcode,
-		FmtR_SSS>;
+		FmtR_SSS,
+		II_FLOAT>;
 
 	def VV : FormatRUnmaskedTwoOpInst<
 		(outs GPR32:$dest),
@@ -563,7 +595,8 @@ multiclass FloatCompareInst<string operator, CondCode condition, bits<6> opcode,
 		operator # "_f $dest, $src1, $src2",
 		[(set i32:$dest, (vectorIntr v16f32:$src1, v16f32:$src2))],
 		opcode,
-		FmtR_VVV>;
+		FmtR_VVV,
+		II_FLOAT>;
 
 	def VS : FormatRUnmaskedTwoOpInst<
 		(outs GPR32:$dest),
@@ -571,7 +604,8 @@ multiclass FloatCompareInst<string operator, CondCode condition, bits<6> opcode,
 		operator # "_f $dest, $src1, $src2",
 		[(set i32:$dest, (vectorIntr v16f32:$src1, (splat f32:$src2)))],
 		opcode,
-		FmtR_VVS>;
+		FmtR_VVS,
+		II_FLOAT>;
 }
 
 //////////////////////////////////////////////////////////////////
@@ -596,7 +630,7 @@ def FmtM_ScGathMasked : FmtM<14>;
 
 class FormatMInst<dag outputs, dag inputs, string asmString, list<dag> pattern,
 	FmtM fmt, bits <1> isLoad>  
-	: NyuziInstruction<outputs, inputs, asmString, pattern> 
+	: NyuziInstruction<outputs, inputs, asmString, pattern, II_MEMORY> 
 {
 	bits <5> srcDest;
 
@@ -668,7 +702,7 @@ def FmtC_MemBar : FmtC<4>;
 
 class FormatCInst<dag outputs, dag inputs, string asmString, list<dag> pattern,
 	FmtC fmt> 
-	: NyuziInstruction<outputs, inputs, asmString, pattern>
+	: NyuziInstruction<outputs, inputs, asmString, pattern, II_MEMORY>
 {
 	let Inst{31-28} = 0xe;
 	let Inst{27-25} = fmt.Value;
@@ -703,7 +737,7 @@ def BT_Eret : BranchType<7>;
 
 class BranchInst<dag outputs, dag inputs, string asmString, list<dag> pattern,
 	BranchType type>
-	: NyuziInstruction<outputs, inputs, asmString, pattern>
+	: NyuziInstruction<outputs, inputs, asmString, pattern, II_BRANCH>
 {
 	let isBranch = 1;
 

lib/Target/Nyuzi/NyuziInstrInfo.td
@@ -40,7 +40,8 @@ def SEXT16 : FormatRUnmaskedOneOpInst<
 	"sext_16 $dest, $src2",
 	[(set GPR32:$dest, (sext_inreg i32:$src2, i16))],
 	0x1e,
-	FmtR_SSS>;
+	FmtR_SSS,
+	II_INT>;
 
 def SEXT8 : FormatRUnmaskedOneOpInst<
 	(outs GPR32:$dest), 
@@ -48,7 +49,8 @@ def SEXT8 : FormatRUnmaskedOneOpInst<
 	"sext_8 $dest, $src2",
 	[(set GPR32:$dest, (sext_inreg i32:$src2, i8))],
 	0x1d,
-	FmtR_SSS>;
+	FmtR_SSS,
+	II_INT>;
 
 // XXX need predicated versions of these
 def ITOFSS : FormatRUnmaskedOneOpInst<
@@ -57,7 +59,8 @@ def ITOFSS : FormatRUnmaskedOneOpInst<
 	"itof $dest, $src2",
 	[(set f32:$dest, (sint_to_fp i32:$src2))],
 	0x2a,
-	FmtR_SSS>;
+	FmtR_SSS,
+	II_FLOAT>;
 
 def ITOFVV : FormatRUnmaskedOneOpInst<
 	(outs VR512:$dest), 
@@ -65,7 +68,8 @@ def ITOFVV : FormatRUnmaskedOneOpInst<
 	"itof $dest, $src2",
 	[(set v16f32:$dest, (sint_to_fp v16i32:$src2))],
 	0x2a,
-	FmtR_VVV>;
+	FmtR_VVV,
+	II_FLOAT>;
 
 def ITOFVS : FormatRUnmaskedOneOpInst<
 	(outs VR512:$dest), 
@@ -73,7 +77,8 @@ def ITOFVS : FormatRUnmaskedOneOpInst<
 	"itof $dest, $src2",
 	[(set v16f32:$dest, (sint_to_fp (splat i32:$src2)))],
 	0x2a,
-	FmtR_VVS>;
+	FmtR_VVS,
+	II_FLOAT>;
 
 def FTOIISS : FormatRUnmaskedOneOpInst<
 	(outs GPR32:$dest), 
@@ -81,7 +86,8 @@ def FTOIISS : FormatRUnmaskedOneOpInst<
 	"ftoi $dest, $src2",
 	[(set i32:$dest, (fp_to_sint f32:$src2))],
 	0x1b,
-	FmtR_SSS>;
+	FmtR_SSS,
+	II_FLOAT>;
 
 def FTOSIVV : FormatRUnmaskedOneOpInst<
 	(outs VR512:$dest), 
@@ -89,7 +95,8 @@ def FTOSIVV : FormatRUnmaskedOneOpInst<
 	"ftoi $dest, $src2",
 	[(set v16i32:$dest, (fp_to_sint v16f32:$src2))],
 	0x1b,
-	FmtR_VVV>;
+	FmtR_VVV,
+	II_FLOAT>;
 
 def FTOSIVS : FormatRUnmaskedOneOpInst<
 	(outs VR512:$dest), 
@@ -97,7 +104,8 @@ def FTOSIVS : FormatRUnmaskedOneOpInst<
 	"ftoi $dest, $src2",
 	[(set v16i32:$dest, (fp_to_sint (splat f32:$src2)))],
 	0x1b,
-	FmtR_VVS>;
+	FmtR_VVS,
+	II_FLOAT>;
 
 defm SGTSI : IntCompareInst<"cmpgt_i", SETGT, 0x12, int_nyuzi_mask_cmpi_sgt>;	
 defm SGESI : IntCompareInst<"cmpge_i", SETGE, 0x13, int_nyuzi_mask_cmpi_sge>;
@@ -123,7 +131,8 @@ def GET_LANEI : FormatRUnmaskedTwoOpInst<
 	"getlane $dest, $src1, $src2",	// XXX this isn't a good name, replace
 	[(set i32:$dest, (extractelt v16i32:$src1, i32:$src2))],
 	0x1a,
-	FmtR_VVS>;
+	FmtR_VVS,
+	II_INT>;
 
 def GET_LANEIimm : FormatIUnmaskedInst<
 	(outs GPR32:$dest),
@@ -131,7 +140,8 @@ def GET_LANEIimm : FormatIUnmaskedInst<
 	"getlane $dest, $src1, $imm",
 	[(set i32:$dest, (extractelt v16i32:$src1, simm13:$imm))],
 	0x1a,
-	FmtI_VV>;
+	FmtI_VV,
+	II_INT>;
 
 def : Pat<(extractelt v16f32:$src1, i32:$src2),
 	(GET_LANEI v16f32:$src1, i32:$src2)>; 
@@ -144,7 +154,8 @@ def SHUFFLEI : FormatRUnmaskedTwoOpInst<
 	"shuffle $dest, $src1, $src2",
 	[(set v16i32:$dest, (int_nyuzi_shufflei v16i32:$src1, v16i32:$src2))],
 	0x0d,
-	FmtR_VVV>;
+	FmtR_VVV,
+	II_INT>;
 	
 let Constraints = "$dest = $oldvalue" in {
 	def SHUFFLEI_MASK : FormatRMaskedTwoOpInst<
@@ -154,7 +165,8 @@ let Constraints = "$dest = $oldvalue" in {
 		[(set v16i32:$dest, (int_nyuzi_vector_mixi i32:$mask, (int_nyuzi_shufflei v16i32:$src1, 
 			v16i32:$src2), v16i32:$oldvalue))],
 		0x0d,
-		FmtR_VVVM>;
+		FmtR_VVVM,
+		II_INT>;
 }
 
 // Floating point shuffle forms
@@ -170,7 +182,8 @@ def MOVESS : FormatRUnmaskedOneOpInst<
 	"move $dest, $src2",
 	[],	// assembler only form for register-to-register moves
 	0xf,
-	FmtR_SSS>;
+	FmtR_SSS,
+	II_INT>;
 
 // This should only be invoked for types that will fit in the immediate field 
 // of the instruction.  The lowering code will transform other types into
@@ -181,7 +194,8 @@ def MOVESimm : FormatIUnmaskedInst<
 	"move $dest, $imm",
 	[(set i32:$dest, imm:$imm)],	// XXX should this be simm13?
 	0x0f,
-	FmtI_SS>;
+	FmtI_SS,
+	II_INT>;
 
 def MOVEVSI : FormatRUnmaskedOneOpInst<
 	(outs VR512:$dest), 
@@ -189,7 +203,8 @@ def MOVEVSI : FormatRUnmaskedOneOpInst<
 	"move $dest, $src2",
 	[(set v16i32:$dest, (splat i32:$src2))],
 	0xf,
-	FmtR_VVS>;
+	FmtR_VVS,
+	II_INT>;
 
 def MOVEVimm : FormatIUnmaskedInst<
 	(outs VR512:$dest), 
@@ -197,7 +212,8 @@ def MOVEVimm : FormatIUnmaskedInst<
 	"move $dest, $imm",
 	[(set v16i32:$dest, (splat imm:$imm))],	// Should this be simm13
 	0xf,
-	FmtI_VS>;
+	FmtI_VS,
+	II_INT>;
 	
 def : Pat<(v16f32 (splat f32:$src2)), (MOVEVSI f32:$src2)>;
 
@@ -207,7 +223,8 @@ def MOVEVV : FormatRUnmaskedOneOpInst<
 	"move $dest, $src2",
 	[],
 	0xf,
-	FmtR_VVV>;
+	FmtR_VVV,
+	II_INT>;
 
 // Predicated
 let Constraints = "$dest = $oldvalue" in {
@@ -217,7 +234,8 @@ let Constraints = "$dest = $oldvalue" in {
 		"move_mask $dest, $mask, $src2",
 		[(set v16i32:$dest, (int_nyuzi_vector_mixi i32:$mask, v16i32:$src2, v16i32:$oldvalue))],
 		0xf,
-		FmtR_VVVM>;
+		FmtR_VVVM,
+		II_INT>;
 
 	def MOVEVVMIimm : FormatIMaskedInst<
 		(outs VR512:$dest),
@@ -225,7 +243,8 @@ let Constraints = "$dest = $oldvalue" in {
 		"move_mask $dest, $mask, $imm",
 		[(set v16i32:$dest, (int_nyuzi_vector_mixi i32:$mask, (splat imm:$imm), v16i32:$oldvalue))],	// simm8?
 		0xf,
-		FmtI_VSM>;
+		FmtI_VSM,
+		II_INT>;
 
 	def MOVEVSMI : FormatRMaskedOneOpInst<
 		(outs VR512:$dest),
@@ -233,7 +252,8 @@ let Constraints = "$dest = $oldvalue" in {
 		"move_mask $dest, $mask, $src2",
 		[(set v16i32:$dest, (int_nyuzi_vector_mixi i32:$mask, (splat i32:$src2), v16i32:$oldvalue))],
 		0xf,
-		FmtR_VVSM>;
+		FmtR_VVSM,
+		II_INT>;
 }
 
 def : Pat<(int_nyuzi_vector_mixf i32:$mask, v16f32:$src2, v16f32:$oldvalue), 
@@ -469,7 +489,8 @@ let isTerminator = 1 in {
 		(outs), 
 		(ins GPR32:$dest),
 		"goto $dest",
-		[(brind i32:$dest)]>
+		[(brind i32:$dest)],
+		II_BRANCH>
 	{
 		bits<5> dest;
 
@@ -491,7 +512,8 @@ let isTerminator = 1 in {
 		(outs), 
 		(ins GPR32:$tableptr, GPR32:$jt),
 		"load_32 pc, ( $tableptr )",
-		[(brjt i32:$tableptr, tjumptable:$jt)]>
+		[(brjt i32:$tableptr, tjumptable:$jt)],
+		II_MEMORY>
 	{
 		let isBranch = 1;
 		let isIndirectBranch = 1;
@@ -517,7 +539,8 @@ def RET : NyuziInstruction<
 	(outs),
 	(ins),
 	"ret",
-	[(return)]>
+	[(return)],
+	II_BRANCH>
 {
 	// This is encoded as move pc, ra
 	let Inst{31-29} = 6;	// format A instruction
@@ -535,7 +558,8 @@ def ERET : NyuziInstruction<
 	(outs),
 	(ins),
 	"eret",
-	[]>
+	[],
+	II_BRANCH>
 {
 	let Inst{31-28} = 0xf;
 	let Inst{27-25} = 7;	// Eret branch type
@@ -653,7 +677,8 @@ def LOAD_EFFECTIVE_ADDR : NyuziInstruction<
 	(outs GPR32:$dest),
 	(ins LEAri:$addr),
 	"lea $dest, $addr",
-	[(set i32:$dest, ADDRri:$addr)]>
+	[(set i32:$dest, ADDRri:$addr)],
+	II_INT>
 {
 	let isAsmParserOnly = 1; 	// Don't disassemble
 
@@ -676,7 +701,8 @@ def LOAD_JTABLE_ADDR : NyuziInstruction<
 	(outs GPR32:$dest),
 	(ins JTADDR:$addr),
 	"lea $dest, $addr",
-	[(set i32:$dest, (jtwrapper tjumptable:$addr))]>
+	[(set i32:$dest, (jtwrapper tjumptable:$addr))],
+	II_INT>
 {
 	let isCodeGenOnly = 1;  // Don't use for assembler or disassembler
   
@@ -694,7 +720,8 @@ def NOP : NyuziInstruction<
 	(outs),
 	(ins),
 	"nop",
-	[]>;
+	[],
+	II_INT>;
 
 // Conversions
 def : Pat<(v16f32 (bitconvert (v16i32 VR512:$src))), (v16f32 VR512:$src)>;

commit 13a9eaf1850da380950c3248f23a5bc15958286c

Fix codegen bug
Disable instruction itineraries introduced in change bee0b24b7bef8de7.
When NyuziInstrInfo inserts instructions to handle large frames, some were
getting removed. It appears enabling the scheduling code was responsible.

lib/Target/Nyuzi/Nyuzi.td
@@ -34,7 +34,9 @@ def NyuziAsmWriter : AsmWriter {
   bit isMCAsmWriter = 1;
 }
 
-def : Processor<"nyuzi", NyuziItineraries, []>;
+// Currently disabled because it causes bad code gen with large stack frames
+//def : Processor<"nyuzi", NyuziItineraries, []>;
+def : Processor<"nyuzi", NoItineraries, []>;
 
 def Nyuzi : Target {
   // Pull in Instruction Info:

lib/Target/Nyuzi/NyuziInstrInfo.cpp
@@ -274,6 +274,7 @@ void NyuziInstrInfo::adjustStackPointer(MachineBasicBlock &MBB,
   }
 }
 
+// Load constant larger than immediate field (13 bits signed)
 unsigned int NyuziInstrInfo::loadConstant(MachineBasicBlock &MBB,
                                           MachineBasicBlock::iterator MBBI,
                                           int Value) const {
@@ -283,29 +284,21 @@ unsigned int NyuziInstrInfo::loadConstant(MachineBasicBlock &MBB,
   unsigned Reg = RegInfo.createVirtualRegister(&Nyuzi::GPR32RegClass);
 
   if (!isInt<24>(Value))
-    report_fatal_error("loadImmediate: unsupported offset");
+    report_fatal_error("NyuziInstrInfo::loadConstant: value out of range");
 
-  if (isInt<13>(Value)) {
-    // Can load directly into this register
-    BuildMI(MBB, MBBI, DL, get(Nyuzi::MOVESimm))
-        .addReg(Reg)
-        .addImm(Value >> 12);
-  } else {
-    // Load bits 23-12 into register
-    BuildMI(MBB, MBBI, DL, get(Nyuzi::MOVESimm), Reg).addImm(Value >> 12);
-    BuildMI(MBB, MBBI, DL, get(Nyuzi::SLLSSI))
-        .addReg(Reg)
-        .addReg(Reg)
-        .addImm(12);
+  BuildMI(MBB, MBBI, DL, get(Nyuzi::MOVESimm), Reg).addImm(Value >> 12);
+  BuildMI(MBB, MBBI, DL, get(Nyuzi::SLLSSI))
+      .addReg(Reg)
+      .addReg(Reg)
+      .addImm(12);
 
-    if ((Value & 0xfff) != 0) {
-      // Load bits 11-0 into register (note we only load 12 bits because we
-      // don't want sign extension)
-      BuildMI(MBB, MBBI, DL, get(Nyuzi::ORSSI))
-          .addReg(Reg)
-          .addReg(Reg)
-          .addImm(Value & 0xfff);
-    }
+  if ((Value & 0xfff) != 0) {
+    // Load bits 11-0 into register (note we only load 12 bits because we
+    // don't want sign extension)
+    BuildMI(MBB, MBBI, DL, get(Nyuzi::ORSSI))
+        .addReg(Reg)
+        .addReg(Reg)
+        .addImm(Value & 0xfff);
   }
 
   return Reg;

lib/Target/Nyuzi/NyuziInstrInfo.h
@@ -79,10 +79,11 @@ public:
 
   void adjustStackPointer(MachineBasicBlock &MBB, MachineBasicBlock::iterator I,
                           int Amount) const;
+
+private:
   unsigned int loadConstant(MachineBasicBlock &MBB,
                             MachineBasicBlock::iterator MBBI, int Amount) const;
 
-private:
   MachineMemOperand *getMemOperand(MachineBasicBlock &MBB, int FI,
                                    unsigned Flag) const;
   const NyuziRegisterInfo RI;

lib/Target/Nyuzi/NyuziSubtarget.cpp
@@ -38,7 +38,6 @@ NyuziSubtarget::NyuziSubtarget(const Triple &TT, const std::string &CPU,
     CPUName = "nyuzi";
 
   InstrItins = getInstrItineraryForCPU(CPUName);
-  assert(!InstrItins.isEmpty());
 
   // Parse features string.
   ParseSubtargetFeatures(CPUName, FS);

lib/Target/Nyuzi/NyuziSubtarget.h
@@ -58,12 +58,14 @@ public:
   virtual const NyuziSelectionDAGInfo *getSelectionDAGInfo() const override {
     return &TSInfo;
   }
-  bool enableMachineScheduler() const override {
-    return true;
-  }
-  bool enablePostRAScheduler() const override {
-    return true;
-  }
+
+// Currently disabled because these cause bad code gen with large stack frames
+//  bool enableMachineScheduler() const override {
+//    return true;
+//  }
+//  bool enablePostRAScheduler() const override {
+//    return true;
+//  }
   const InstrItineraryData *getInstrItineraryData() const override {
     return &InstrItins;
   }