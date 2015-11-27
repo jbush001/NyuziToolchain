aa91f5834c09eb6af7cd531457a3ab18e33c4b3d
Jeff Bush <jeffbush001@gmail.com>
2015-11-26 17:52:11 -0800

    Cleanup
    - Remove stray project() call in CMakeLists.txt (merge artifact)
    - Stub in support for Nyuzi in ELFYAML. I'm not sure how to test this,
    but it only involves recognizing the EM_ macro and including the relocations.
    - Remove some changes originally made to CMakeLists.txt in mclinker project
    to fix a compile error. The latest version works without these.

--- lib/Object/ELFYAML.cpp
+++ lib/Object/ELFYAML.cpp
@@ -194,6 +194,7 @@ ScalarEnumerationTraits<ELFYAML::ELF_EM>::enumeration(IO &IO,
   ECase(EM_78KOR)
   ECase(EM_56800EX)
   ECase(EM_AMDGPU)
+  ECase(EM_NYUZI)
 #undef ECase
 }
 
@@ -506,6 +507,9 @@ void ScalarEnumerationTraits<ELFYAML::ELF_REL>::enumeration(
     break;
   case ELF::EM_ARM:
 #include "llvm/Support/ELFRelocs/ARM.def"
+    break;
+  case ELF::EM_NYUZI:
+#include "llvm/Support/ELFRelocs/Nyuzi.def"
     break;
   default:
     llvm_unreachable("Unsupported architecture");
