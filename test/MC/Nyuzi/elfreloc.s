772ddde5d5cd7ec133ed6f0689b7afa308e64db1
Jeff Bush <jeffbush001@gmail.com>
2016-07-09 07:22:54 -0700

    Add more checks to elfreloc.s test

--- test/MC/Nyuzi/elfreloc.s
+++ test/MC/Nyuzi/elfreloc.s
@@ -1,18 +1,22 @@
 # RUN: llvm-mc -filetype=obj -triple nyuzi-elf %s -o - | \
 # RUN: llvm-objdump -r - | FileCheck %s
 
+# CHECK: RELOCATION RECORDS
 
-foo: goto foo	# The fixup for this will be resolved internally and will not create a
-				# relocation.
+# These fixups will be resolved internally and will not create relocations.
+foo: goto foo           # fixup_Nyuzi_PCRel_Branch
+    # CHECK-NOT: foo
+boo: load_32 s5, boo    # fixup_Nyuzi_PCRel_MemAccExt
+    # CHECK-NOT: boo
+bah: lea s3, bah        # fixup_Nyuzi_PCRel_ComputeLabelAddress
+    # CHECK-NOT: bah
+buz: .long buz          # FK_Data_4
+    # CHECK-NOT: buz
 
-	# CHECK: RELOCATION RECORDS
-call memcpy
-	# CHECK: 00000004 R_NYUZI_BRANCH memcpy
-call exit
-	# CHECK: 00000008 R_NYUZI_BRANCH exit
-.long ioctl
-	# CHECK: 0000000c R_NYUZI_ABS32 ioctl
-load_32 s0, bar
-    # CHECK: 00000010 R_NYUZI_PCREL_MEM_EXT bar
-lea s1, baz
-    # CHECK: 00000014 R_NYUZI_PCREL_LEA baz
+# These fixups reference labels that are not defined here. Check that they
+# create relocations.
+
+goto memcpy     # CHECK: 00000010 R_NYUZI_BRANCH memcpy
+load_32 s0, bar # CHECK: 00000014 R_NYUZI_PCREL_MEM_EXT bar
+lea s1, baz     # CHECK: 00000018 R_NYUZI_PCREL_LEA baz
+.long ioctl     # CHECK: 0000001c R_NYUZI_ABS32 ioctl
