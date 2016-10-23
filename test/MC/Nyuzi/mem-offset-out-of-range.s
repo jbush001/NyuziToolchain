8d9f3c46082efa6efa91ee4611a69ffc5b547ce2
Jeff Bush <jeffbush001@gmail.com>
2016-10-22 18:32:03 -0700

    Tweak memory offset test to check boundary

--- test/MC/Nyuzi/mem-offset-out-of-range.s
+++ test/MC/Nyuzi/mem-offset-out-of-range.s
@@ -4,65 +4,65 @@
 # Masked immediate offset is 10 bits
 
 load_32 s0, 0x3fff(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-load_32 s0, -0x3fff(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-load_32 s0, 0xffff(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
-load_32 s0, -0xffff(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+load_32 s0, -0x4000(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
+load_32 s0, 0x4000(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+load_32 s0, -0x4001(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
 load_sync s0, 0x3fff(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-load_sync s0, -0x3fff(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-load_sync s0, 0xffff(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
-load_sync s0, -0xffff(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+load_sync s0, -0x4000(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
+load_sync s0, 0x4000(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+load_sync s0, -0x4001(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
 
 store_32 s0, 0x3fff(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-store_32 s0, -0x3fff(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-store_32 s0, 0xffff(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
-store_32 s0, -0xffff(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+store_32 s0, -0x4000(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
+store_32 s0, 0x4000(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+store_32 s0, -0x4001(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
 store_sync s0, 0x3fff(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-store_sync s0, -0x3fff(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-store_sync s0, 0xffff(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
-store_sync s0, -0xffff(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+store_sync s0, -0x4000(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
+store_sync s0, 0x4000(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+store_sync s0, -0x4001(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
 
-load_s16 s0, 0xffff(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+load_s16 s0, 0x4000(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
 load_u16 s0, 0x3fff(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-store_16 s0, 0xffff(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+store_16 s0, 0x4000(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
 store_16 s0, 0x3fff(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
 
-load_s8 s0, 0xffff(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+load_s8 s0, 0x4000(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
 load_u8 s0, 0x3fff(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-store_8 s0, 0xffff(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+store_8 s0, 0x4000(s1) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
 store_8 s0, 0x3fff(s1) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
 
 load_v v0, 0x3fff(s0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-load_v v0, -0x3fff(s0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-load_v v0, 0x7fff(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
-load_v v0, -0x7fff(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+load_v v0, -0x4000(s0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
+load_v v0, 0x4000(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+load_v v0, -0x4001(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
 store_v v0, 0x3fff(s0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-store_v v0, -0x3fff(s0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-store_v v0, 0x7fff(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
-store_v v0, -0x7fff(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+store_v v0, -0x4000(s0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
+store_v v0, 0x4000(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+store_v v0, -0x4001(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
 
 load_v_mask v0, s0, 0x1ff(s0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-load_v_mask v0, s0, -0x1ff(s0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-load_v_mask v0, s0, 0x3ff(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
-load_v_mask v0, s0, -0x3ff(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+load_v_mask v0, s0, -0x200(s0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
+load_v_mask v0, s0, 0x200(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+load_v_mask v0, s0, -0x201(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
 store_v_mask v0, s0, 0x1ff(s0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-store_v_mask v0, s0, -0x1ff(s0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-store_v_mask v0, s0, 0x3ff(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
-store_v_mask v0, s0, -0x3ff(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+store_v_mask v0, s0, -0x200(s0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
+store_v_mask v0, s0, 0x200(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+store_v_mask v0, s0, -0x201(s0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
 
 load_gath v0, 0x3fff(v0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-load_gath v0, -0x3fff(v0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-load_gath v0, 0x7fff(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
-load_gath v0, -0x7fff(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+load_gath v0, -0x4000(v0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
+load_gath v0, 0x4000(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+load_gath v0, -0x4001(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
 store_scat v0, 0x3fff(v0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-store_scat v0, -0x3fff(v0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-store_scat v0, 0x7fff(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
-store_scat v0, -0x7fff(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+store_scat v0, -0x4000(v0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
+store_scat v0, 0x4000(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+store_scat v0, -0x4001(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
 
 load_gath_mask v0, s0, 0x1ff(v0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-load_gath_mask v0, s0, -0x1ff(v0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-load_gath_mask v0, s0, 0x3ff(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
-load_gath_mask v0, s0, -0x3ff(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+load_gath_mask v0, s0, -0x200(v0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
+load_gath_mask v0, s0, 0x200(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+load_gath_mask v0, s0, -0x201(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
 store_scat_mask v0, s0, 0x1ff(v0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-store_scat_mask v0, s0, -0x1ff(v0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
-store_scat_mask v0, s0, 0x3ff(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
-store_scat_mask v0, s0, -0x3ff(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+store_scat_mask v0, s0, -0x200(v0) # CHECK-NOT: [[@LINE]]:{{[0-9]+}}: error
+store_scat_mask v0, s0, 0x200(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
+store_scat_mask v0, s0, -0x201(v0) # CHECK: mem-offset-out-of-range.s:[[@LINE]]:{{[0-9]+}}: error: offset out of range
