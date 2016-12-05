5b73291758556d5392e13a9cf39c03ca017e82f8
Jeff Bush <jeffbush001@gmail.com>
2016-12-04 21:36:11 -0800

    Fix failing test case
    False negative.  Removed check for label which didn't seem to be used anyway.

--- tools/spmd-compile/test/for-loop.c
+++ tools/spmd-compile/test/for-loop.c
@@ -15,7 +15,6 @@ float f(float count)
 // CHECK: .LBB0_2:
 // CHECK: 	add_f_mask v3, s1, v3, s0
 // CHECK: 	add_f_mask v1, s1, v1, v2
-// CHECK: .LBB0_1:
 // CHECK: 	cmplt_f s1, v2, v0
 // CHECK: 	bfalse s1, .LBB0_3
 // CHECK: 	goto .LBB0_2
