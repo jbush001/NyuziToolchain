8f9de80261031eb318eea09cbb44e15d0b38804a
Jeff Bush <jeffbush001@gmail.com>
2017-07-15 13:21:12 -0700

    Issue #87: Add assembler mnemonic spell corrector support

--- test/MC/Nyuzi/unrecognized-mnemonic.s
+++ test/MC/Nyuzi/unrecognized-mnemonic.s
@@ -1,3 +1,9 @@
 # RUN: not llvm-mc -filetype=obj -triple nyuzi-elf  %s -o /dev/null 2>&1 | FileCheck  %s
 
-flargbh s0, s1, s2  # CHECK: unrecognized-mnemonic.s:[[@LINE]]:{{[0-9]+}}: error: unrecognized instruction mnemonic
+flargbh s0, s1, s2  # CHECK: unrecognized-mnemonic.s:[[@LINE]]:{{[0-9]+}}: error: invalid instruction
+
+# Check that it suggests properly when the mnemonic is close
+load_33 s0, (s1)    # CHECK: unrecognized-mnemonic.s:[[@LINE]]:{{[0-9]+}}: error: invalid instruction, did you mean: load_32?
+ad_i s0, s1, s2     # CHECK: unrecognized-mnemonic.s:[[@LINE]]:{{[0-9]+}}: error: invalid instruction, did you mean: add_i?
+subb_i s0, s1, s2    # CHECK: unrecognized-mnemonic.s:[[@LINE]]:{{[0-9]+}}: error: invalid instruction, did you mean: sub_i?
+
