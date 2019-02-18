2cd9842772cd0326befd6c26d82093ed3ee156e5
Jeff Bush <jeffbush001@gmail.com>
2019-02-17 19:30:46 -0800

    Issue #105: Support full hex file format in elf2hex
    Add support for @ in hex files.

--- tools/elf2hex/elf2hex.cpp
+++ tools/elf2hex/elf2hex.cpp
@@ -22,26 +22,55 @@
 using namespace llvm::ELF;
 using namespace llvm;
 
-static cl::opt<std::string>
+namespace {
+
+cl::opt<std::string>
 InputFilename(cl::Positional, cl::desc("<input ELF file>"), cl::init("a.out"));
 
-static cl::opt<std::string> OutputFilename("o", cl::desc("Output hex file"),
+cl::opt<std::string> OutputFilename("o", cl::desc("Output hex file"),
                                            cl::value_desc("filename"));
-static cl::opt<unsigned int> BaseAddress("b", cl::desc("Base Address"),
-                                           cl::init(0));
+
+int writeHex(FILE *outputFile, FILE *inputFile, int length) {
+  for (int i = 0; i < length; i++) {
+      int c = fgetc(inputFile);
+      if (c == EOF) {
+        errs() << "Unexpected end of file\n";
+        return -1;
+      }
+
+      fprintf(outputFile, "%02x", c);
+      if ((i & 3) == 3)
+        fprintf(outputFile, "\n");
+  }
+
+  return 0;
+}
+
+int writeZeros(FILE *outputFile, int length) {
+  for (int i = 0; i < length / 4; i++) {
+    if (fprintf(outputFile, "00000000\n") < 0) {
+      errs() << "Error writing to output file\n" << strerror(errno) << "\n";
+      return -1;
+    }
+  }
+
+  return 0;
+}
+
+}  // namespace
 
 int main(int argc, const char *argv[]) {
   cl::ParseCommandLineOptions(argc, argv, "elf2hex converter\n");
 
   FILE *inputFile = fopen(InputFilename.c_str(), "rb");
   if (!inputFile) {
-    errs() << "Error opening input file\n";
+    errs() << "Error opening input file: " << strerror(errno) << "\n";;
     return 1;
   }
 
   Elf32_Ehdr eheader;
   if (fread(&eheader, sizeof(eheader), 1, inputFile) != 1) {
-    errs() << "Error reading header\n";
+    errs() << "Error reading header: " << strerror(errno) << "\n";
     return 1;
   }
 
@@ -60,70 +89,63 @@ int main(int argc, const char *argv[]) {
     return 1;
   }
 
-  Elf32_Phdr *pheader = (Elf32_Phdr *)calloc(sizeof(Elf32_Phdr), eheader.e_phnum);
+  Elf32_Phdr *pheader = (Elf32_Phdr*) calloc(sizeof(Elf32_Phdr), eheader.e_phnum);
   fseek(inputFile, eheader.e_phoff, SEEK_SET);
   if (fread(pheader, sizeof(Elf32_Phdr), eheader.e_phnum, inputFile) !=
       eheader.e_phnum) {
-    errs() << "error reading program header\n";
+    errs() << "error reading program header " << strerror(errno) << "\n";
     return 1;
   }
 
-  // Walk throught the segments and find the highest address
-  unsigned int maxAddress = 0;
-  for (int segment = 0; segment < eheader.e_phnum; segment++) {
-    if (pheader[segment].p_type == PT_LOAD) {
-      unsigned int highAddr = pheader[segment].p_vaddr + pheader[segment].p_memsz;
-      if (highAddr > maxAddress)
-        maxAddress = highAddr;
-
-      if (pheader[segment].p_vaddr < BaseAddress) {
-        errs() << "Program segment " << segment << " @" << pheader[segment].p_vaddr
-  		    << " is before requested base address " << BaseAddress << "\n";
-        return 1;
-      }
-    }
-  }
-
-  unsigned char *memoryImage = (unsigned char *)calloc(maxAddress - BaseAddress, 1);
-  if (!memoryImage) {
-    errs() << "not enough memory for program image\n";
+  FILE *outputFile = fopen(OutputFilename.c_str(), "wb");
+  if (!outputFile) {
+    errs() << "Error opening output file: " << strerror(errno) << "\n";
     return 1;
   }
 
+  fprintf(outputFile, "// Converted from %s by elf2hex\n", InputFilename.c_str());
+
+  bool first = true;
   for (int segment = 0; segment < eheader.e_phnum; segment++) {
     if (pheader[segment].p_type == PT_LOAD) {
+      fprintf(outputFile, "@%08x\n", pheader[segment].p_vaddr / 4);
       if (fseek(inputFile, pheader[segment].p_offset, SEEK_SET) < 0) {
-        errs() << "Error reading segment " << segment << "\n";
+        errs() << "Error reading segment " << segment << ": "
+          << strerror(errno) << "\n";
         return 1;
       }
 
-      if (fread(memoryImage + pheader[segment].p_vaddr - BaseAddress, 1,
-                pheader[segment].p_filesz,
-                inputFile) != pheader[segment].p_filesz) {
-        errs() << "Error reading segment " << segment << "\n";
+      int sizeToCopy = pheader[segment].p_filesz;
+      if (first) {
+        // Replace the first word with a jump instruction to the
+        // appropriate location
+        // XXX assumes loadable segments are ordered by virtual address
+        int offset = eheader.e_entry - pheader[segment].p_vaddr;
+        uint32_t instruction = 0xf6000000 | (offset / 4);
+        fprintf(outputFile, "%02x%02x%02x%02x\n",
+          instruction & 0xff,
+          (instruction >> 8) & 0xff,
+          (instruction >> 16) & 0xff,
+          (instruction >> 24) & 0xff);
+        sizeToCopy -= 4;
+        fseek(inputFile, 4, SEEK_CUR);
+        first = false;
+      }
+
+      if (writeHex(outputFile, inputFile, sizeToCopy) < 0) {
         return 1;
+      }
+
+      int zeroFill = pheader[segment].p_memsz - pheader[segment].p_filesz;
+      if (zeroFill > 0) {
+        if (writeZeros(outputFile, zeroFill) < 0) {
+          return 1;
+        }
       }
     }
   }
 
   fclose(inputFile);
-
-  // Convert the first word into a jump instruction to the appropriate location
-  *((unsigned int *)memoryImage) = 0xf6000000 | ((eheader.e_entry
-      - BaseAddress) / 4);
-
-  FILE *outputFile = fopen(OutputFilename.c_str(), "wb");
-  if (!outputFile) {
-    errs() << "Error opening output file";
-    return 1;
-  }
-
-  for (unsigned int i = 0; i < maxAddress - BaseAddress; i++) {
-    fprintf(outputFile, "%02x", memoryImage[i]);
-    if ((i & 3) == 3)
-      fprintf(outputFile, "\n");
-  }
-
   fclose(outputFile);
 
   return 0;
